e67e832d2d147b1ca28d8b4a69f4eb8a92cbbbab
witold <witoldwrob@gmail.com>
2023-02-02 17:07:38 +0100

    feat(gui): add extract type choice & lst config file
    feat(gui): let the user pick configuration file
    
    fix(gui): crash when no config file

--- gui.py
+++ gui.py
@@ -1,29 +1,50 @@
-# Inspired from https://doc.qt.io/qtforpython/examples/example_network__downloader.html
-
 import pathlib
 import sys
 
 from PySide6.QtCore import QDir, QThread, Signal, Slot
-from PySide6.QtWidgets import (QApplication, QFileDialog, QHBoxLayout,
-                               QLineEdit, QMessageBox, QPushButton, QStyle,
-                               QTextEdit, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (
+    QApplication,
+    QButtonGroup,
+    QCheckBox,
+    QFileDialog,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QMessageBox,
+    QPushButton,
+    QStyle,
+    QTextEdit,
+    QVBoxLayout,
+    QWidget,
+)
 
-from converter import convert_globals_to_hdf5
+from converter import ExtractionType, convert
 
 
 class ConverterWorker(QThread):
 
     finished_signal = Signal(int)
+    failed_signal = Signal(Exception)
 
     data_path: pathlib.Path | None = None
     output_path: pathlib.Path | None = None
+    extraction_types: tuple[ExtractionType, ...] = None
+    lst_config_path: pathlib.Path | None = None
 
     def __init__(self):
         super().__init__()
 
     def run(self):
-        num_processed_files = convert_globals_to_hdf5(self.data_path, self.output_path)
-        self.finished_signal.emit(num_processed_files)
+        try:
+            processed_files_num = convert(
+                self.extraction_types,
+                self.data_path,
+                self.output_path,
+                self.lst_config_path,
+            )
+        except Exception as error:
+            self.failed_signal.emit(error)
+        self.finished_signal.emit(processed_files_num)
 
 
 class ConverterWidget(QWidget):
@@ -32,8 +53,11 @@ class ConverterWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.setWindowTitle("NewAGLAE HDF5 converter")
+
         self.worker = ConverterWorker()
         self.worker.finished_signal.connect(self.on_conversion_completed)
+        self.worker.failed_signal.connect(self.on_conversion_failure)
 
         self.start_button = QPushButton("Start")
         self.start_button.setDisabled(True)
@@ -51,27 +75,29 @@ class ConverterWidget(QWidget):
         ).triggered.connect(self.on_dest_path_click)
 
         self.context_box = QTextEdit()
-        self.context_box.setDisabled(True)
+        self.context_box.setReadOnly(True)
         self.context_box.setText(
             "Select source & destination paths then press start button."
         )
 
         # buttons bar layout
-        hlayout = QHBoxLayout()
-        hlayout.addStretch()
-        hlayout.addWidget(self.start_button)
+        buttonslayout = QHBoxLayout()
+        buttonslayout.addStretch()
+        buttonslayout.addWidget(self.start_button)
 
-        contexlayout = QVBoxLayout()
-        contexlayout.addStretch()
-        contexlayout.addWidget(self.context_box)
+        self.checkboxeslayout = ExtractionTypeChexboxesLayout()
 
         # main layout
         vlayout = QVBoxLayout(self)
         vlayout.addWidget(self.source_path_box)
         vlayout.addWidget(self.dest_path_box)
-        vlayout.addStretch()
-        vlayout.addLayout(hlayout)
-        vlayout.addLayout(contexlayout)
+        vlayout.addSpacing(12)
+        vlayout.addLayout(self.checkboxeslayout)
+        vlayout.addSpacing(12)
+        self.config_file_input_layout = ConfigFileInputLayout()
+        vlayout.addLayout(self.config_file_input_layout)
+        vlayout.addLayout(buttonslayout)
+        vlayout.addWidget(self.context_box)
 
         self.resize(600, 300)
 
@@ -106,14 +132,30 @@ class ConverterWidget(QWidget):
         self.start_button.setDisabled(True)
         self.worker.output_path = dest_path
         self.worker.data_path = source_path
+        self.worker.extraction_types = self.checkboxeslayout.selected_extractions
+        if self.config_file_input_layout.config_file:
+            self.worker.lst_config_path = pathlib.Path(
+                self.config_file_input_layout.config_file
+            )
         self.worker.start()
+        self.context_box.append(
+            "Selected extraction types : {}.".format(
+                ", ".join(etype.name.lower() for etype in self.worker.extraction_types)
+            )
+        )
         self.context_box.append("Conversion has started.")
 
     @Slot()
-    def on_conversion_completed(self, num_processed_files: int):
+    def on_conversion_completed(self, processed_files_num):
         self.start_button.setDisabled(False)
+        self.context_box.append(f"Processed {processed_files_num} files.")
         self.context_box.append("Done.")
-        self.context_box.append("Processed %s files." % num_processed_files)
+
+    @Slot()
+    def on_conversion_failure(self, error: Exception):
+        self.start_button.setDisabled(False)
+        self.context_box.append(f"An error occured : {error} ({type(error).__name__})")
+        raise type(error) from error
 
     @Slot()
     def on_source_path_click(self):
@@ -141,6 +183,65 @@ class ConverterWidget(QWidget):
         if self.dest_path_box.text() and self.source_path_box.text():
             self.start_button.setDisabled(False)
             self.context_box.append("Ready ?")
+
+
+class ExtractionTypeChexboxesLayout(QHBoxLayout):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.checkboxes: dict[ExtractionType, QCheckBox] = {
+            ExtractionType.LST: QCheckBox("LST"),
+            ExtractionType.GLOBALS: QCheckBox("Globals"),
+            ExtractionType.STANDARDS: QCheckBox("Standards"),
+        }
+
+        title = QLabel()
+        title.setText("Extraction type")
+        self.addWidget(title)
+
+        extract_choice_group = QButtonGroup()
+        extract_choice_group.setExclusive(False)
+        for checkbox in self.checkboxes.values():
+            checkbox.setChecked(True)
+            extract_choice_group.addButton(checkbox)
+            self.addWidget(checkbox)
+
+    @property
+    def selected_extractions(self) -> tuple[ExtractionType, ...]:
+        return tuple(
+            extraction
+            for extraction, checkbox in self.checkboxes.items()
+            if checkbox.isChecked()
+        )
+
+
+class ConfigFileInputLayout(QVBoxLayout):
+    def __init__(self):
+        super().__init__()
+        title = QLabel()
+        title.setText("Config file (optional)")
+        self.addWidget(title)
+
+        self.config_path_box = QLineEdit()
+        self.config_path_box.setPlaceholderText("Run data folder path")
+        self.config_path_box.addAction(
+            qApp.style().standardIcon(QStyle.SP_DirOpenIcon), QLineEdit.TrailingPosition
+        ).triggered.connect(self.on_path_click)
+        self.addWidget(self.config_path_box)
+
+    @property
+    def config_file(self) -> str | None:
+        return self.config_path_box.text() or None
+
+    @Slot()
+    def on_path_click(self):
+        file_path = QFileDialog.getOpenFileName(
+            caption="Open configuration file",
+            dir=QDir.homePath(),
+            filter="Config Files (*.yaml, *.yml)",
+        )
+        if file_path:
+            self.config_path_box.setText(file_path[0])
 
 
 if __name__ == "__main__":
