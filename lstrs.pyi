8d83c98b228206a35d06ca2020445d8ca8fd4592
Maxime Cattet <maximecattet@gmx.com>
2023-04-03 15:35:06 +0200

    style: Run format python code

--- lstrs.pyi
+++ lstrs.pyi
@@ -9,6 +9,8 @@ class LstConfig:
     y: int
     detectors: dict[str, Detector]
 
-    def __init__(self, x: int, y: int, detectors: dict[str, Detector], computed_detectors: dict[str, list[str]]) -> None: ...
+    def __init__(
+        self, x: int, y: int, detectors: dict[str, Detector], computed_detectors: dict[str, list[str]]
+    ) -> None: ...
 
 def parse_lst(filename: str, output_path: str, config: LstConfig) -> None: ...
